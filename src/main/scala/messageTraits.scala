40bfddbfa988a1d345052e6a269de8dfb5f3fd23
Cameron Meyer <cameron@convergencelabs.com>
2018-05-25 16:23:41 -0700

    updated package

--- src/main/scala/messageTraits.scala
+++ src/main/scala/messageTraits.scala
@@ -1,3 +1,15 @@
-package com.convergencelabs
+package com.convergencelabs.protocol
 
-trait Blah
+trait Incoming
+trait Outgoing
+trait Request
+trait Response
+
+trait Activity
+trait Authentication
+trait Chat
+trait Historical
+trait Identity
+trait Model
+trait Permissions
+trait Presence
